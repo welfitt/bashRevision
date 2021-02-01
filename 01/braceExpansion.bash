aa2105ac12a9a1c9b63a815b02d5e08b1f4b5bac
Adrian Nicholls <welfitt@gmail.com>
2021-02-01 10:57:16 +0000

    First commit

---
+++ 01/braceExpansion.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+echo "This will give you bins barns and ballons"
+echo b{i,ar,alloo}ns
+
+echo "You can even nest this stuff "
+echo b{ar{d,n,k},ed}s
+
+echo "And you can generate sequences..." {1..8} " " {a..z}
